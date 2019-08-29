2a3cf783d092cb508329866d329fb538948237d3
takashi0602 <takashioicsn@gmail.com>
2019-08-29 18:17:26 +0900

    モデル 追加

---
+++ app/Model/Dao/Explanation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Model\Dao;
+
+/**
+ * Class Explanation
+ *
+ * Explanationテーブルを扱う Classです
+ * DAO.phpに用意したCRUD関数以外を実装するときに、記載をします。
+ *
+ */
+class Explanation extends Dao
+{
+
+}
---
+++ app/Model/Dao/Video.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Model\Dao;
+
+/**
+ * Class Video
+ *
+ * Videoテーブルを扱う Classです
+ * DAO.phpに用意したCRUD関数以外を実装するときに、記載をします。
+ *
+ */
+class Video extends Dao
+{
+
+}
