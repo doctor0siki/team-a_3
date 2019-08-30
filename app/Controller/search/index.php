ce6e47da61ad5ec233378debc5c3682efc4fa0c9
takashi0602 <takashioicsn@gmail.com>
2019-08-30 14:48:51 +0900

    検索機能 作成

--- app/Controller/search/index.php
+++ app/Controller/search/index.php
@@ -2,13 +2,15 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Model\Dao\Video;
 
 // TOPページのコントローラ
 $app->get('/result', function (Request $request, Response $response) {
 
-    $data = [];
-    $data = $request->getQueryParams();
+    $query = $request->getQueryParams();
+    $video = new Video($this->db);
+    $data["list"] = $video->search($query["search"]);
 
     // Render index view
     return $this->view->render($response, 'top/index.twig', $data);
-});
+})->setName('result');
--- app/Controller/top/index.php
+++ app/Controller/top/index.php
@@ -2,13 +2,13 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Model\Dao\Video;
 
 // TOPページのコントローラ
 $app->get('/', function (Request $request, Response $response) {
 
-    $data = [];
-    $data['video_id'] = "0";
-    $data['user_id'] = "0";
+    $video = new Video($this->db);
+    $data["list"] = $video->getVideoList();
 
     // Render index view
     return $this->view->render($response, 'top/index.twig', $data);
